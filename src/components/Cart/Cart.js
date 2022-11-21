8be2b5ff2006c53ef38d4097597ce3549c90a15c
ANJAAAAN <96495449+ANJAAAAN@users.noreply.github.com>
2022-11-21 14:05:45 +0545

    updated CartButton &CartItem

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/Cart-Context";
+import CartItem from "./CartItem";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 export default function Cart(props) {
-  const caritems = (
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `Rs ${cartCtx.totalAmount.toFixed(2)}`;
+
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = id=>{
+    cartCtx.removeItem(id);
+   
+  };
+
+  //For adding item sinside cart
+  const cardItemHandler= item =>{
+    cartCtx.addItem({...item,amount:1});
+  };
+
+  const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((items) => (
-        <li>{items.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount = {item.amount}
+          price={item.price}
+          //  here bind insures thata the added or the removed item is passed through these function.
+          onRemove ={cartItemRemoveHandler.bind(null,item.id)}
+          onAdd={cardItemHandler.bind(null ,item)}
+        ></CartItem>
       ))}
+      {/* {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((items) => (
+        <li>{items.name}</li>
+      ))} */}
     </ul>
   );
   return (
     <Modal onClose={props.onClose}>
-      {caritems}
+      {cartItems}
       <div className={classes.total}>
         <span> Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button-alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from '../../store/Cart-Context'
 import classes from "./HeaderCartButton.module.css";
 
 export default function HeaderCartButton(props) {
+  const [btnBump,setBtnBump]=useState(false);
 
   // here using udeContext the HeaderCertButoon component will be reevaluated by React when ever the context changes.
     const cartCtx = useContext(CartContext);
     // here reduce is a build in method that allows to transform array of data into a single value.
-    const numberOfCardItems = cartCtx.items.reduce((curNumber,item)=>{
+    const {items} = cartCtx;
+    const numberOfCardItems = items.reduce((curNumber,item)=>{
       return curNumber + item.amount;
-    },0);
-  return (
+    },0); 
+
+    const btnClasses = `${classes.button} ${btnBump?classes.bump:''}`;
+
     
-        <button className={classes.button} onClick={props.onClick}>
+
+    useEffect(()=>{
+      if(items.length ===0){
+        return;
+      }
+      setBtnBump(true);
+
+     const timer= setTimeout(() => {
+        setBtnBump(false);
+      }, 300);
+       return()=>{
+        clearTimeout(timer);
+       } 
+    },[items]);
+  return (
+
+        <button className={btnClasses} onClick={props.onClick}>
       {/* first span is for wrapping icons which is comming from CartIcon folder */}
       <span className={classes.icon}>
         <CartIcon></CartIcon>
